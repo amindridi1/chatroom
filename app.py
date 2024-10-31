bea34c6668b3b06e361dcec5c27ddbcfc2f4a93d
amindridi1 <125443150+amindridi1@users.noreply.github.com>
2024-10-31 20:27:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def handle_message(msg):
     send(msg, broadcast=True)  # Broadcasts message to all users
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0", port=8000)
