60853adba9f51f468d6b013b18e9d986e4234751
amindridi1 <125443150+amindridi1@users.noreply.github.com>
2024-10-31 19:59:20 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+    return render_template('chat.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message: {msg}")
+    send(msg, broadcast=True)  # Broadcasts message to all users
+
+if __name__ == '__main__':
+    socketio.run(app)
